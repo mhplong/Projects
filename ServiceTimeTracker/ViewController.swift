4029d9e85ca3b8b897f2aa2feab70d3ef8de1a9e
Mark Long <mhplong@windstream.net>
2016-05-11 21:55:50 -0500

    Initial Commit

---
+++ ServiceTimeTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ServiceTimeTracker
+//
+//  Created by Mark Long on 5/11/16.
+//  Copyright © 2016 Mark Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
